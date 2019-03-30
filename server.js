b669f6074184026529652e22feaa9203d627b6c2
Alex Yepes <alexisyepes@yahoo.com>
2019-03-30 18:03:12 -0400

    db config

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newscraperdb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/newscraperdb", { useNewUrlParser: true });
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newscraperdb";
+
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds237955.mlab.com:37955/heroku_2sn8tw5g");
 
 // Routes
 app.get("/", function (req, res) {
