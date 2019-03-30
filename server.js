65d6f5d400280bcc344103bef9ecb9b5e147e3bc
Alex Yepes <alexisyepes@yahoo.com>
2019-03-30 14:51:48 -0400

    Final files

---
+++ server.js
@@ -0,0 +1,106 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/newscraperdb", { useNewUrlParser: true });
+
+// Routes
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+// A GET route for scraping the BBC website
+app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.bbc.com/news/science_and_environment").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    $(".title-link").each(function (i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .children(".title-link__title")
+        .text();
+      result.link = $(this)
+        .attr("href");
+      result.summary = $(this).next()
+        .text();
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+
+    res.render("index");
+  });
+});
+
+app.get("/articles", function (req, res) {
+  db.Article.find({})
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+     
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
